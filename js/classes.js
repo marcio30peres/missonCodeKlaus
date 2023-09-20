2d69d6477fc04cb01833e44b633b590931f87c6d
marciolppdev <marcio30peres@gmail.com>
2023-09-20 11:02:56 -0300

    add: santa class

--- js/classes.js
+++ js/classes.js
@@ -13,3 +13,29 @@ export class Neighbourhood {
     this.height = height
   }
 }
+
+export class Santa {
+  constructor(posX, posY, giftsCarried) {
+    this.posX = posX
+    this.posY = posY
+    this.giftsCarried = giftsCarried
+  }
+  moveUp() {
+    this.posY = this.posY + 1
+  }
+  moveDown() {
+    this.posY = this.posY - 1
+  }
+  moveRight() {
+    this.posX = this.posX + 1
+  }
+  moveLeft() {
+    this.posX = this.posX - 1
+  }
+  pickGift() {
+    this.giftsCarried = this.giftsCarried + 1
+  }
+  deliverGift() {
+    this.giftsCarried = this.giftsCarried + 1
+  }
+}
